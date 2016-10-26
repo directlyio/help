66f3a01b90231d7a442b19af18b79139325345b2
Dylan <dylanslack@gmail.com>
2016-10-26 14:42:47 -0600

    Add temporary site base path

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,18 +2,24 @@ import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './styles.scss';
 
+const getBaseSitePath = () => 'https://directly.io';
+
+// FIXME: implement once envs are created
+
+/*
 const getBaseSitePath = () => {
   switch (process.env.NODE_ENV) {
     case 'production':
-      return 'https://directly.io/search';
+      return 'https://directly.io';
     case 'staging':
-      return 'https://directly-staging.surge.sh/search';
+      return 'https://directly-staging.surge.sh';
     default:
       return 'http://localhost:3000';
   }
 };
+*/
 
-const Header = ({ id }) => (
+const Header = () => (
   <header className={styles.header}>
     <div className={styles.headerContainer}>
       <div
